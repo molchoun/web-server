6e81b3d4282a93fb3f9e4d270df00dc1ec32e40a
molchoun <movsisian.arsen@gmail.com>
2022-05-19 14:29:50 +0400

    Submit form.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -11,3 +11,12 @@ def home():
 @app.route('/<string:page_name>')
 def works(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        return redirect('/thank_you.html')
+    else:
+        return 'something went wrong'
