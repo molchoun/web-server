fb79d776b39bd1f261c272f29a5ab99c86eb01d9
molchoun <movsisian.arsen@gmail.com>
2022-05-19 14:43:26 +0400

    Write submit form data in database.txt

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-
+import csv
 app = Flask(__name__)
 
 
@@ -9,14 +9,22 @@ def home():
 
 
 @app.route('/<string:page_name>')
-def works(page_name):
+def index(page_name):
     return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
+        write_to_file(data)
         return redirect('/thank_you.html')
     else:
         return 'something went wrong'
