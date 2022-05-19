d149575a986946d91a1b76e2a7136f2a381e531a
molchoun <movsisian.arsen@gmail.com>
2022-05-19 15:35:56 +0400

    Using csv module to write submit form data in database.csv

--- server.py
+++ server.py
@@ -12,19 +12,29 @@ def home():
 def index(page_name):
     return render_template(page_name)
 
+
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
         email = data['email']
         subject = data['subject']
         message = data['message']
-        file = database.write(f'\n{email},{subject},{message}')
+        file = database.write(f'\n{email}   {subject}   {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', newline='', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thank_you.html')
     else:
         return 'something went wrong'
